8eae937c307a0b23d3890603ddd343badad34692
s-channah <hannahsadofabohns@gmail.com>
2024-06-07 05:48:10 +0100

    initialised the variables for measuring heights

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l_height = 0;
+	size_t r_height = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (l_height > r_height ? l_height : r_height);
 }
